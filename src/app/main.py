71a72a4f5a3438ed456d2ed4016328801406df4f
jab9814 <barreto.jose9814@gmail.com>
2025-07-10 18:22:22 -0400

    Agregado autorizacion en root como ejemplo

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from app.routers import customers, invoices, transactions, plans
 from db_sqlite import create_all_tables
 
 
 app = FastAPI(lifespan=create_all_tables)
 
+security = HTTPBasic()
+
 @app.get("/")
-async def root():
-    return {"message": "root"}
+async def root(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    if credentials.username == 'username' and credentials.password == 'password':
+        return {"message": "Autorizado"}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
 app.include_router(customers.router)
 app.include_router(invoices.router)
