6a470f7c7f17655ba5cf9077b28c9000b5743135
jab9814 <barreto.jose9814@gmail.com>
2025-07-10 17:34:56 -0400

    agregado middleware log_request_time

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-from app.routers import customers, invoices, transactions, plans
+import time
+from fastapi import FastAPI, Response
 from db_sqlite import create_all_tables
+from app.routers import customers, invoices, transactions, plans
 
 
 app = FastAPI(lifespan=create_all_tables)
@@ -13,3 +14,12 @@ app.include_router(customers.router)
 app.include_router(invoices.router)
 app.include_router(transactions.router)
 app.include_router(plans.router)
+
+
+@app.middleware('http')
+async def log_request_time(request: Response, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(f"[LOG - INFO] -> Request: [{request.url}] completed in [{process_time:4f}] seconds")
+    return response
